a7c2ade69566ec5a9635fd399f5d641ee043181f
Wewleee <106530667+Wewleee@users.noreply.github.com>
2026-05-28 03:45:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# Это нужно, чтобы браузер (фронтенд) мог общаться с сервером
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -12,5 +12,5 @@ app.add_middleware(
 )
 
 @app.get("/")
-def read_root():
-    return {"message": "Привет! Сервер работает!"}
+async def read_index():
+    return FileResponse('index.html')
